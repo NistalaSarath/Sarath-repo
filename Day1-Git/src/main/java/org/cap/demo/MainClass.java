eaf38c6659582fa540b885be16bd1c35e6d4038c
snistala <snistala@DIN73003004.corp.capgemini.com>
2018-08-06 10:52:59 +0530

    My second Commit

--- Day1-Git/src/main/java/org/cap/demo/MainClass.java
+++ Day1-Git/src/main/java/org/cap/demo/MainClass.java
@@ -9,6 +9,10 @@ public class MainClass {
 	}
 	public static void greet() {
 		System.out.println("Hello");
+		System.out.println("Good Morning");
+		System.out.println("Good Afternoon");
+		System.out.println("Good Evening");
+		System.out.println("Good Night");
 	}
 
 }
