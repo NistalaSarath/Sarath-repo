95d3c7c117d8743d460eb1db089bf11182da4bd2
vamaddin <vamaddin@DIN73002976.corp.capgemini.com>
2018-08-06 15:01:29 +0530

    sum removed

--- Day1-Git/src/main/java/org/cap/demo/SalesClass.java
+++ Day1-Git/src/main/java/org/cap/demo/SalesClass.java
@@ -5,11 +5,11 @@ public class SalesClass {
 	
 	public void show()
 	{
-		int dum=0;
+		
 		for(int num=0;num<arr[num];num++)  {
-			dum+=num;
+			
 			System.out.println(num);
-		}		System.out.println(dum);
+		}		
 
 	}
 }
