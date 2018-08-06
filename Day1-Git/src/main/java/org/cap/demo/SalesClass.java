4d232cdc3f8a0d9f309a1e1c4cb972c768b144ae
vamaddin <vamaddin@DIN73002976.corp.capgemini.com>
2018-08-06 14:28:41 +0530

    showmethod included

--- Day1-Git/src/main/java/org/cap/demo/SalesClass.java
+++ Day1-Git/src/main/java/org/cap/demo/SalesClass.java
@@ -1,12 +1,15 @@
 package org.cap.demo;
 
 public class SalesClass {
-	int[] arr= {1,2,3,-89,56};
+	int[] arr= {4,2,3,-89,56};
 	
 	public void show()
 	{
+		int sum=0;
 		for(int num:arr) {
+			sum+=num;
 			System.out.println(num);
-		}
+		}		System.out.println(sum);
+
 	}
 }
